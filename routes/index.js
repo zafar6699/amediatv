dc2fcef87fdf6b0350dbffd6d643fe0f9ff42b49
Zafar6699 <webzafar6699@gmail.com>
2021-03-29 12:17:50 +0500

    a

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {Home} = require('../controllers/indexController')
+const {Home} = require('../controllers/indexController_z')
 
 router.get('/', Home)
 
