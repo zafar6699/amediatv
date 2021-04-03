9e6d21249d5a1176e264ad17b1ac78888e1fd8e2
shahriyor isomiddinov <shahriyornodejs@mail.ru>
2021-04-03 14:48:35 +0500

    dsad

--- controllers/indexController_z.js
+++ controllers/indexController_z.js
@@ -3,6 +3,39 @@ const Janr = require("../models/janr")
 const Slider = require('../models/slider')
 
 exports.Home = async (req, res) => {
+    // const slider = await Slider.find()
+    //     .sort({date: -1})
+    //     .populate({path: 'kino',select: ['name','image','screens','description','rating']})
+    //     .populate({path: 'serial',select: ['name','image','screens','description','rating']})
+
+    
+    // const news = await News.find()
+    //     .sort({date: -1})
+    //     .limit(3)
+    //     .select(['name','date','image'])
+    // const anotatsiya = await Anotatsiya.find({status: true})
+    //     .sort({date: -1})
+    //     // .limit(1)
+    // const kino = await Kino.find()
+    //     // .limit(20)
+    //     .sort({date: -1})
+    //     .select({name: 1, category: 1, image: 1, rating: 1,year: 1, janr: 1,date: 1,description: 1, price:1})
+    //     .populate({path: 'category', select: 'nameuz'})
+    //     .populate(['janr'])
+    // const season = await Season.find()
+    //     // .limit(20)
+    //     .sort({date: -1})
+    //     .select({name: 1, category: 1, image: 1, rating: 1,year: 1, janr: 1,date: 1, num: 1, description: 1, price:1})
+    //     .populate({path: 'category', select: 'nameuz'})
+    //     .populate(['janr'])
+
+  
+
+
+
+
+
+
     const janr = await Janr.find().sort({createdAt: - 1})
     const slider = await Slider.find().sort({date: - 1})
 
