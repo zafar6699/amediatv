59f3b356851f997448ab5127ce7755277457e77f
Zafar6699 <webzafar6699@gmail.com>
2021-04-07 17:05:35 +0500

    s

--- routes/comment.js
+++ routes/comment.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const { writeComment, allComments } = require('../controllers/comment');
 const router = express.Router({ mergeParams: true });
+const Comment = require("../models/comment")
+const CommentSeason = require("../models/commentSerial")
 
 router.post("/kino", async (req, res) => {
     const comment = await Comment.create(req.body);
     
-    res.status(201).json({success: true, data: comment})
+    res.redirect(`/kino/${req.body.kinoId}`)
+})
+router.post("/season", async (req, res) => {
+    const comment = await CommentSeason.create(req.body);
+    
+    res.redirect(`/season/${req.body.season}`)
+
 })
 
-router.route('/add')
-        .post(writeComment);
-router.get('/all', allComments)
+// router.route('/add')
+//         .post(writeComment);
+// router.get('/all', allComments)
 
 // router.route('/:id')
 //         .put(protect, authorize('admin', 'publisher'), editStatus)
