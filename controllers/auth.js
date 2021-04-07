d38b35bad347f1cea7fb8ab444d877b5b11d0e38
shahriyor isomiddinov <shahriyornodejs@mail.ru>
2021-04-07 12:49:22 +0500

    SAd

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,8 +13,8 @@ exports.register = async (req, res, next) => {
     })
     await user.save()
         .then(() => {
-            res.status(201).json({ success: true, data: user });
-            // res.redirect('/')
+            // res.status(201).json({ success: true, data: user });
+            res.redirect('/')
         })
         .catch((error) => {
             // res.status(400).json({ success: false, data: error });
@@ -39,8 +39,8 @@ exports.login = async (req, res, next) => {
     const body = await User.findOne({ email: req.body.email })
     req.session.user = body
     req.session.save()
-    res.status(200).json({ success: true, data: body });
-    // res.redirect('/')
+    // res.status(200).json({ success: true, data: body });
+    res.redirect('/')
 
 }
 exports.getSession = async (req, res) => {
