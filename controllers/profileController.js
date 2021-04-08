90670f5847f868b896d896b660f825c1570d1e7f
shahriyor isomiddinov <shahriyornodejs@mail.ru>
2021-04-08 12:41:42 +0500

    sdas

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,9 +1,10 @@
 const Janr = require("../models/janr")
 const priceList = require('../models/priceList')
+const comments = require('../models/')
 exports.Profile = async (req, res) => {
     const janr = await Janr.find().sort({ createdAt: - 1 })
     const list = await priceList.find().sort({ date: -1 })
-    
+    const 
     res.render("./main/profile", {
         title: "Profile",
         layout: "layout",
