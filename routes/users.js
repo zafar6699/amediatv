29eacff16cff05b0d7139d3cf22416308c798d9c
shahriyor isomiddinov <shahriyornodejs@mail.ru>
2021-04-14 18:53:25 +0500

    asdsadA

--- routes/users.js
+++ routes/users.js
@@ -14,6 +14,6 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
-router.put('/upload/:id', upload.single('photo'), updateFile);
+router.post('/:id', upload.single('photo'), updateFile);
 
 module.exports = router;
