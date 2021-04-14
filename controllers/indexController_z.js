31ed4b4e1e45048abe95e06a7c351e478373fcd1
shahriyor isomiddinov <shahriyornodejs@mail.ru>
2021-04-14 16:44:29 +0500

    asdsA

--- controllers/indexController_z.js
+++ controllers/indexController_z.js
@@ -12,43 +12,59 @@ exports.Home = async (req, res) => {
 
     let sortKino = []
 
-    
-     category.forEach(async (element) => {
-        let s = await Kino.find({ category: { $all: [element._id] } }).select({name: 1, image: 1, price: 1});
-        sortKino.push(s);        
+
+    category.forEach(async (element) => {
+        let s = await Kino.find({ category: { $all: [element._id] } }).select({ name: 1, image: 1, price: 1 });
+        sortKino.push(s);
     });
-    
+
+
     const janr = await Janr.find()
 
-    const slider = await Slider.find()
-        .sort({date: -1})
-        .populate({path: 'kino',select: ['name','image','screens','description','rating']})
-        // .populate({path: 'serial',select: ['name','image','screens','description','rating']})
+    // const slider = await Slider.find()
+    //     .sort({ date: -1 })
+    //     .populate({ path: 'kino', select: ['name', 'image', 'screens', 'description', 'rating'] })
+    // // .populate({path: 'serial',select: ['name','image','screens','description','rating']})
 
-    const oneKino = await Kino.find().sort({date: -1}).limit(1)
-        .select({name: 1, category: 1, image: 1, rating: 1,year: 1, janr: 1,date: 1,description: 1, video:1})
-        .populate({path: 'category', select: 'nameuz'})
+    let slider = await Slider.find()
+        .sort({ date: -1 })
+        .populate(
+            {
+                path: 'kino',
+                select: ['name', 'image', 'screens','description', 'rating']
+            }
+        )
+        .populate(
+            {
+                path: 'serial',
+                select: ['name', 'image', 'screens','description', 'rating']
+            }
+        )
+
+    const oneKino = await Kino.find().sort({ date: -1 }).limit(1)
+        .select({ name: 1, category: 1, image: 1, rating: 1, year: 1, janr: 1, date: 1, description: 1, video: 1 })
+        .populate({ path: 'category', select: 'nameuz' })
         .populate(['janr'])
 
     const news = await News.find()
-        .sort({date: -1})
+        .sort({ date: -1 })
         .limit(3)
-        .select(['name','date','image'])
+        .select(['name', 'date', 'image'])
 
     const kino = await Kino.find()
-        .sort({date: -1})
-        .select({name: 1, category: 1, url: 1, image: 1, rating: 1,year: 1, janr: 1,date: 1,description: 1, price:1})
-        .populate({path: 'category', select: 'nameuz'})
+        .sort({ date: -1 })
+        .select({ name: 1, category: 1, url: 1, image: 1, rating: 1, year: 1, janr: 1, date: 1, description: 1, price: 1 })
+        .populate({ path: 'category', select: 'nameuz' })
         .populate(['janr'])
     const serial = await Season.find()
         .limit(4)
-        .sort({date: -1})
+        .sort({ date: -1 })
         // .select({name: 1, category: 1, image: 1, rating: 1,year: 1, janr: 1,date: 1, num: 1, description: 1, price:1})
-        .populate({path: 'category', select: 'nameuz'})
+        .populate({ path: 'category', select: 'nameuz' })
         .populate(['janr'])
 
-    
-  
+
+
 
     res.render("./main/index", {
         title: "Home",
@@ -56,23 +72,23 @@ exports.Home = async (req, res) => {
         user: req.session.user, lang: req.session.ulang,
         serial, janr, slider, oneKino, news, kino, category, sortKino
     })
-    
+
 }
 
 exports.OneJanr = async (req, res) => {
-    res.render("./main/onejanr", {title: "Home"})
+    res.render("./main/onejanr", { title: "Home" })
 }
 exports.Janrs = async (req, res) => {
     const result = new Janr(req.body);
     await result.save()
-    .then(response => {
-        res.status(200).json({
-            data: response 
+        .then(response => {
+            res.status(200).json({
+                data: response
+            })
         })
-    })
-    .catch(error => {
-        res.status(400).json({
-            error: error 
+        .catch(error => {
+            res.status(400).json({
+                error: error
+            })
         })
-    })
 }
