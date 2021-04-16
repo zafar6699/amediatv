7642ed5e66a19c8fa2ce6986f98519ec0944eb9b
shahriyor isomiddinov <shahriyornodejs@mail.ru>
2021-04-16 11:54:29 +0500

    sda

--- routes/categories.js
+++ routes/categories.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {
     getQuery,
     getCategories,
-    getCategory,SortAllCategoryKinoSeason
+    getCategory,SortAllCategoryKinoSeason,
     createCategory,
     getByJanr
 
