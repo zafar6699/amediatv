ea9a7cd16a2c94a03e95da3af3c0c8e758ec9ab8
shahriyor isomiddinov <shahriyornodejs@mail.ru>
2021-04-17 12:16:47 +0500

    ASD

--- controllers/AddCommentSeason.js
+++ controllers/AddCommentSeason.js
@@ -1,36 +1,39 @@
 const Comment = require('../models/AddCommentSeason')
+const CommentSeason = require('../models/commentSerial')
 
 exports.writeComment = async (req, res, next) => {
- const user = req.session.user;
- const comment = new Comment({
-  message: req.body.message,
-  prevComment: req.body.prevComment,
-  userID: user._id,
- })
- await comment.save()
-  .then(() => {
-   res.json({ data: comment })
+  const user = req.session.user;
+  const comment = new Comment({
+    message: req.body.message,
+    prevComment: req.body.prevComment,
+    userID: user._id,
   })
-  .catch((error) => {
-   res.json({ data: error })
-  })
+  await comment.save()
+    .then(() => {
+      res.json({ data: comment })
+    })
+    .catch((error) => {
+      res.json({ data: error })
+    })
 }
 
 exports.getOne = async (req, res) => {
- const comment = await Comment.findById({_id: req.body.id})
-  .populate({
-   path: 'userID', select: ['name']
-  })
- 
- res.json(comment)
+  const comment = await Comment.findById({ _id: req.body.id })
+    .populate({
+      path: 'userID', select: ['name']
+    })
+
+  res.json(comment)
 }
 
 
 exports.getAll = async (req, res) => {
- const comment = await Comment.find({prevComment: req.params.id}).sort({date: -1})
-  .populate({
-   path: 'userID', select: 'name'
-  })
- 
- res.json(comment)
+  const commentSeason = await CommentSeason.find()
+  const comSeas = commentSeason
+  const comment = await Comment.find({ prevComment: comSeas._id }).sort({ date: -1 })
+    .populate({
+      path: 'userID', select: 'name'
+    })
+
+  res.json(comment)
 }
