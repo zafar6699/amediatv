91aba0fbc1f295b62ae94114b081ad2680d10686
shahriyor isomiddinov <shahriyornodejs@mail.ru>
2021-04-17 13:08:50 +0500

    ASD

--- controllers/AddCommentSeason.js
+++ controllers/AddCommentSeason.js
@@ -27,7 +27,7 @@ exports.getOne = async (req, res) => {
 
 
 exports.getAll = async (req, res) => {
-  const comment1 = await Comment.find({ prevComment: req.params.id }).sort({ date: -1 })
+  const comment1 = await Comment.find(prevComment).sort({ date: -1 })
     .populate({
       path: 'userID', select: 'name'
     })
