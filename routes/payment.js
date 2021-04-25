458a9c228b0cd4f3f98c71247e2e3c9e3dbdcf48
shahriyor isomiddinov <shahriyornodejs@mail.ru>
2021-04-25 21:42:19 +0500

    ASd

--- routes/payment.js
+++ routes/payment.js
@@ -2,10 +2,9 @@ const express = require('express')
 const router = express.Router()
 const {checkUser, saveData, Events,checkOson,payOson} = require('../controllers/Payment')
 
-
+router.post('/check/:id/:code', checkUser)
 router.post('/check', checkOson)
 router.post('/pay',payOson)
-router.post('/check/:id/:code', checkUser)
 router.post('/create', saveData)
 router.get('/all', Events)
 
